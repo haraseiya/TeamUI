e8e2a59bf5bfae1d6dfaf2e0ce09c9817e2daaab
haraseiya <seiyamidori@gmail.com>
2019-05-25 09:23:10 +0900

    シーン移動

---
+++ UI/Assets/SceneChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+	public void Picture1()
+    {
+        SceneManager.LoadScene("Picture 1");
+    }
+
+    public void Pucture2()
+    {
+        SceneManager.LoadScene("Picture 2");
+    }
+
+    public void Pucture3()
+    {
+        SceneManager.LoadScene("Picture 3");
+    }
+}
